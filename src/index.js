8d59f99837dd4076adb3036791d4ba1726c2aabb
Vitali Ahel <nonsens_bill@mail.ru>
2022-08-09 13:23:35 +0300

    docs: finish task

--- src/index.js
+++ src/index.js
@@ -39,7 +39,62 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const spaceMorse = '**********'
+    const arrStr = expr.split(spaceMorse);
+    let arrEnd = [];
+    for (let i = 0; i < arrStr.length; i ++) {
+        arrEnd.push(convertArrToWord(convertStrToArr(converToFullWord(arrStr[i]))));
+    }
+    return arrEnd.join(' ');
 }
+
+function converToFullWord (str) {
+    if (str.length % 10 == 0) {
+        return str;
+    } else if (str.length < 10) {
+        return (('0'.repeat(10 - str.length)) + str);
+    } else {
+        return ('0'.repeat(10 - (str.length % 10)) + str)
+    }
+}
+
+function convertStrToArr (str) {
+    const max = str.length / 10;
+    let arr = [];
+    for (let i = 1; i <= max; i++) {
+        arr.push(str.slice((i * 10 - 10),  (i * 10)));
+    }
+    return arr
+}
+
+function convertStrToMorse (str) {
+    let string = '';
+    for (let i = 0; i < str.length; i = i + 2){
+        if ((str[i] + str[i + 1]) == '10') {
+           string = string + '.'     
+        } else if ((str[i] + str[i + 1]) == '11') {
+            string = string + '-'
+        };
+    }
+    return string
+}
+
+function convertMorseToSymbol (str) {
+    return MORSE_TABLE[str];
+}
+
+function convertArrToWord (arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str = str + convertMorseToSymbol(convertStrToMorse(arr[i]));
+    }
+    return str
+}
+
+
+
+
+
 
 module.exports = {
     decode
